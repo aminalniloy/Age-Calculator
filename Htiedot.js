bac47f9142c953bab4cc278e66246c0aa7bf17f5
Al Amin <108260763+aminalniloy@users.noreply.github.com>
2023-02-06 07:31:27 +0200

    Add files via upload

---
+++ Htiedot.js
@@ -0,0 +1,11 @@
+﻿function Laskeikä() 
+{   var enimi = document.getElementById("enimi").value;     
+    var snimi = document.getElementById("snimi").value;
+
+    var spvm = new Date(document.getElementById("spvm").value);
+    var tämäpäivä = new Date();
+    var ikä = tämäpäivä.getFullYear() - spvm.getFullYear();
+
+    var result =  "Hei " + enimi + " " + snimi + ", olet " + ikä + " vuotias!"   
+    document.getElementById("result").value = result; 
+}
